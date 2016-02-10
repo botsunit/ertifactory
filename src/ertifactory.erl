879260258f04767ea111483869fafe6d905f6f52
Mathias Franck <franck.mathias@gmail.com>
2016-02-10 22:04:49 +0100

    Set specific artifactory options \n(props) into the url string

--- src/ertifactory.erl
+++ src/ertifactory.erl
@@ -1,6 +1,7 @@
 -module(ertifactory).
--export([deploy/4, get_deployed_artifact/4]).
--include_lib("eunit/include/eunit.hrl").
+-export([deploy/4, get_deployed_artifact/4, other_options_to_string/1]).
+
+-define(ERTIFACTORY_OPTIONS,[username, password, api_key, path]).
 
 % @doc
 % Gets a given artifact from an Artifactory repository.
@@ -13,13 +14,17 @@ get_deployed_artifact(_BaseUrl, _Repository, _Package, _Options) ->
 
 % @doc
 % Stores an artifact into a given repository.
-%
+% <li><tt>BaseUrl</tt> : URL string of an artifactory server</li>
+% <li><tt>Repository</tt> : Repository name of your repo in the artifactory server </li>
+% <li><tt>Package</tt> : Local path to the artifact file to deploy</li>
+% <li><tt>Options</tt> A list of {Key,Value} options. See below.</li>
 % Availables options :
 % <ul>
-% <li><tt>username</tt></li>
-% <li><tt>password</tt></li>
-% <li><tt>api_key</tt></li>
-% <li><tt>path</tt></li>
+% <li><tt>{username, "my_user_name"} </tt> : username of an artifactory account (Basic Auth.)</li>
+% <li><tt>{password, "my_password"} </tt> : associated password</li>
+% <li><tt>{api_key, "my_api_key} </tt> : Alternatively, the api_key used to connect the account.</li>
+% <li><tt>{path, "my/path/to/artifact"}</tt> : Path and name of the artifact in the repository; 
+% if omitted, the last path component of "Package" will be used</li>
 % </ul>
 % @end
 -spec deploy(BaseUrl :: string(), Repository :: string(), Package :: string(), Options :: term() ) ->
@@ -48,14 +53,14 @@ deploy(BaseUrl, Repository, Package, Options) ->
           BaseUrl ++ "/" ++ Repository ++ "/" ++ PackageName;
         true -> 
           BaseUrl ++ "/" ++ Repository ++ "/" ++ OptPath ++ "/" ++ PackageName
-      end,
+      end ++ other_options_to_string(Options),
       _ = case file:read_file(Package) of
         {ok, Body} ->
           case httpc:request(put, {URL, Header, "application/x-gzip", Body}, [{ssl, [{verify, 0}]}], []) of
             {ok, {{_, 201, _}, _, _}} -> 
               ok;
             {ok, {{_, Code, Message}, _, _}} -> 
-               {error, {Code, Message}};
+              {error, {Code, Message}};
             OtherReturn ->  
               OtherReturn
           end;
@@ -64,7 +69,18 @@ deploy(BaseUrl, Repository, Package, Options) ->
       end;
     Err ->
       Err
-end.
+  end.
+
+other_options_to_string(Options) ->
+  lists:flatten(lists:filtermap(
+  fun({K, V}) ->
+    case lists:member(K,?ERTIFACTORY_OPTIONS) of
+      false -> 
+        {true, io_lib:format(";~s=~s", [K,V])};
+      true -> false
+    end
+  end,
+  Options)).
 
 ensure_net_started() ->
   case application:ensure_all_started(inets) of
